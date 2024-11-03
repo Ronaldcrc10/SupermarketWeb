93fbc73a05881d3e7b5f7d738e1ea1a84f574698
Ronaldo <ronaldcollo172@gmail.com>
2024-11-03 18:12:05 -0500

    Index Categories

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        public IList<Category> Categories { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if(_context.Categories != null)
+            {
+                Categories = await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
