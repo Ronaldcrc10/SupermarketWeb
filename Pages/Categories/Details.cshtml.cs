c4631993328cb02a55fb804cb08e50222361d773
Ronaldo <ronaldcollo172@gmail.com>
2024-11-04 17:35:48 -0500

    Categories Crud Completely

---
+++ Pages/Categories/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWeb.Data;
+using SupermarketWeb.Models;
+
+namespace SupermarketWeb.Pages.Categories
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SupermarketWeb.Data.SupermarketContext _context;
+
+        public DetailsModel(SupermarketWeb.Data.SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category = category;
+            }
+            return Page();
+        }
+    }
+}
